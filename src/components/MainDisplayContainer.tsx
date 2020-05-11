0b72b7c469e22baac9bd6b75a091378c604a3da6
mzzlillieee <tanglillian11@gmail.com>
2020-05-11 14:50:40 -0700

    minor styling changes

--- src/components/MainDisplayContainer.tsx
+++ src/components/MainDisplayContainer.tsx
@@ -11,21 +11,21 @@ const MainDisplayContainer: React.FC<MainDisplayContainerProps> = ({ period, bal
   const mainDisplayView = balance > 0 ? "display-container--positive" : "display-container--negative"
 
   return (
-      <div id="main-display-container">
-        <h1>{period}</h1>
-        <div className={mainDisplayView}>
-          <div className='mascot-wrapper'>
-            <img src={avocadoLogo} alt="Logo" />
+    <div id="main-display-container">
+      <h1>{period}</h1>
+      <div className={mainDisplayView}>
+        <div className='mascot-wrapper'>
+          <img src={avocadoLogo} alt="Logo" />
+        </div>
+        <div className='progress-bar-wrapper'>
+          <h1>${balance} </h1><p style={{ display: "inline" }}>spent</p>
+          <div className='progress-bar-light-grey'>
+            <div className='progress-bar-green'></div>
           </div>
-          <div className='progress-bar-wrapper'>
-            <h1>${balance} </h1><p style={{ display: "inline" }}>spent</p>
-            <div className='progress-bar-light-grey'>
-              <div className='progress-bar-green'></div>
-            </div>
-            <p>$34 leftover</p>
-          </div>
+          <p>$34 left</p>
         </div>
       </div>
+    </div>
   )
 }
 
