ce50816c9c3853fab1c80abf4b5f4df1a1035556
mzzlillieee <tanglillian11@gmail.com>
2020-05-11 14:45:39 -0700

    add a progress bar in MainDisplay component

--- src/components/MainDisplayContainer.tsx
+++ src/components/MainDisplayContainer.tsx
@@ -5,25 +5,27 @@ import avocadoLogo from '../theme/avocado.png'
 interface MainDisplayContainerProps {
   period: string;
   balance: number;
-
 }
 
 const MainDisplayContainer: React.FC<MainDisplayContainerProps> = ({ period, balance }) => {
   const mainDisplayView = balance > 0 ? "display-container--positive" : "display-container--negative"
 
   return (
-    <>
       <div id="main-display-container">
         <h1>{period}</h1>
         <div className={mainDisplayView}>
           <div className='mascot-wrapper'>
             <img src={avocadoLogo} alt="Logo" />
           </div>
-          <div className='chart-wrapper'>sdfsdfs</div>
+          <div className='progress-bar-wrapper'>
+            <h1>${balance} </h1><p style={{ display: "inline" }}>spent</p>
+            <div className='progress-bar-light-grey'>
+              <div className='progress-bar-green'></div>
+            </div>
+            <p>$34 leftover</p>
+          </div>
         </div>
       </div>
-
-    </>
   )
 }
 
