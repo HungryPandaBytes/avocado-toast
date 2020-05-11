912c36a02b9c600bf4c15e4a6ba99684a0f37f13
mzzlillieee <tanglillian11@gmail.com>
2020-05-11 15:33:58 -0700

    add minor styling

--- src/components/MainDisplayContainer.tsx
+++ src/components/MainDisplayContainer.tsx
@@ -13,9 +13,8 @@ const MainDisplayContainer: React.FC<MainDisplayContainerProps> = ({ period, bal
   const displayStyling = balance > 0 ? "display-container display-container--positive" : "display-container display-container--negative"
   const progressbarStyling = balance > 0 ? "progress-bar-green" : "progress-bar-brown"
   const mascot = balance > 0 ? avocadoLogo : burntToastLogo
-  const progressBarPercentage = spent / (balance + spent)
+  const progressBarPercentage = Math.ceil(spent / (balance + spent) * 100)
 
-  console.log(progressBarPercentage)
 
   return (
     <div id="main-display-container">
@@ -27,7 +26,7 @@ const MainDisplayContainer: React.FC<MainDisplayContainerProps> = ({ period, bal
         <div className='progress-bar-wrapper'>
           <h1>${spent}</h1><p style={{ display: "inline" }}> spent</p>
           <div className='progress-bar-light-grey'>
-            <div className={progressbarStyling} style={{ width: `${progressBarPercentage * 100}%` }}></div>
+            <div className={progressbarStyling} style={{ width: `${progressBarPercentage}%` }}></div>
           </div>
           <p>${balance} left</p>
         </div>
