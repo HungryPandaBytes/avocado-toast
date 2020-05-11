2e8003eab7da7c67af625a3e71d6b09ce02cc82e
Lorraine Chan <lchan912@gmail.com>
2020-05-10 23:04:53 -0700

    add #addTransaction

--- src/store.tsx
+++ src/store.tsx
@@ -3,23 +3,27 @@ import React from 'react';
 /* mobx global state */
 import { useLocalStore } from 'mobx-react'
 
-interface contextInterface {
+interface stateInterface {
   transactions: string[],
   budget: { income: number, reoccuringExpenses: number, savingPercentage: number }
 }
 
+interface dispatchInterface {
+  addTransaction(transaction: string): void
+}
+
+interface contextInterface extends stateInterface, dispatchInterface {};
+
 const defaultContext: contextInterface = {
   transactions: [],
-  budget: { income: 0, reoccuringExpenses: 0, savingPercentage: 0 }
+  budget: { income: 0, reoccuringExpenses: 0, savingPercentage: 0 },
+  addTransaction: (transaction) => defaultContext.transactions.push(transaction)
 }
 
 export const StoreContext = React.createContext(defaultContext);
 
 export const StoreProvider = ({ children }: any) => {
-  const store = useLocalStore(() => ({
-    transactions: ['adsfasdf'],
-    budget: { income: 0, reoccuringExpenses: 0, savingPercentage: 0 }
-  }))
+  const store = useLocalStore(() => (Object.assign({}, defaultContext, {transactions: ['tessst']})))
 
   return (
     <StoreContext.Provider value={store}> {children}</StoreContext.Provider >
