aa924512bb620a8990603812b4fae5136088ec7b
mzzlillieee <tanglillian11@gmail.com>
2020-05-11 18:40:42 -0700

    add transaction component and transaction list component

---
+++ src/components/TransactionItem.tsx
@@ -0,0 +1,104 @@
+import React, { useRef, useState } from "react";
+import Moment from 'react-moment';
+
+import {
+  IonItem,
+  IonLabel,
+  IonIcon,
+  IonNote,
+  IonItemSliding,
+  IonItemOptions,
+  IonItemOption,
+  IonModal,
+} from "@ionic/react";
+import {
+  basketOutline,
+  logoAmazon,
+  carOutline,
+  cafeOutline,
+} from "ionicons/icons";
+
+interface TransactionItemProps {
+  transaction: any;
+}
+const TransactionItem: React.FC<TransactionItemProps> = ({
+  transaction
+}) => {
+  const ionItemSlidingRef = useRef<HTMLIonItemSlidingElement>(null);
+  const transactionColor = transaction.ignore ? "medium" : "default"
+  const transactionIconColor = transaction.ignore ? "medium" : getIconColor('basket-outline')
+
+  const ignoreHandler = () => {
+    ionItemSlidingRef.current && ionItemSlidingRef.current.close();
+  }
+
+  // TODO: need category from backend to map correct icon to the transaction
+  transaction = { ...transaction, iconName: "basket-outline" };
+  return (
+
+    <IonItemSliding ref={ionItemSlidingRef} key={transaction.id}>
+      <IonItem lines="inset">
+        <IonIcon
+          color={transactionIconColor}
+          icon={getIcon(transaction.iconName)}
+          key={transaction.id}
+        ></IonIcon>
+        <IonLabel style={{ "border-left": "2px", "padding-left": "10px" }} color={transactionColor}
+        >
+          <h3>{transaction.description}</h3>
+          <IonNote>
+            <p>
+              <Moment fromNow>{transaction.transaction_time}</Moment>
+            </p>
+          </IonNote>
+        </IonLabel>
+        <IonLabel class="ion-text-right" style={{ marginRight: "30px" }} color={transactionColor}
+        >
+          -$
+          {parseInt(transaction.amount).toLocaleString(navigator.language, {
+            minimumFractionDigits: 0,
+          })}
+        </IonLabel>
+      </IonItem>
+      <IonItemOptions>
+        <IonItemOption
+          onClick={ignoreHandler}
+          color="medium"
+        >
+          Ignore
+        </IonItemOption>
+        {/* TODO: update user id for deleteTransaction  */}
+        <IonItemOption
+          color="danger"
+        >
+          Remove
+        </IonItemOption>
+      </IonItemOptions>
+    </IonItemSliding >
+
+  );
+};
+
+export default TransactionItem;
+
+const getIcon: any = (iconName: string) => {
+  const mapping: any = {
+    "basket-outline": basketOutline,
+    "logo-amazon": logoAmazon,
+    "car-outline": carOutline,
+    "cafe-outline": cafeOutline,
+  };
+
+  return mapping[iconName];
+};
+
+const getIconColor: any = (iconName: string) => {
+  const mapping: any = {
+    "basket-outline": "danger",
+    "logo-amazon": "success",
+    "car-outline": "warning",
+    "cafe-outline": "tertiary",
+  };
+
+  return mapping[iconName];
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import { IonContent, IonPage, IonSlides, IonSlide, IonList, IonButton } from '@ionic/react';
+import React, { useRef, useState } from 'react';
+import { IonContent, IonPage, IonSlides, IonSlide, IonList, IonButton, IonListHeader, IonIcon, IonItem, IonLabel } from '@ionic/react';
 import './HomePage.scss';
 import { StoreContext } from '../store'
 import MainDisplayContainer from '../components/MainDisplayContainer'
+import { chevronUpOutline } from 'ionicons/icons';
+import TransactionItem from '../components/TransactionItem';
+
+const transactions = [{ id: 1, amount: 23, description: "Restaurant", iconName: "logo-amazon", transaction_time: new Date() }, { id: 1, amount: 23, description: "Restaurant", iconName: "logo-amazon", transaction_time: new Date() }, { id: 1, amount: 23, description: "Restaurant", iconName: "logo-amazon", transaction_time: new Date() }]
 
 
 const slideOpts = {
@@ -13,6 +17,10 @@ const slideOpts = {
 
 const HomePage: React.FC = () => {
   const store = React.useContext(StoreContext);
+  const headerRef = useRef<HTMLIonListHeaderElement>(null);
+  const transactionListRef = useRef<HTMLIonListElement>(null);
+  const [showAddTransactionModal, setShowAddTransactionModal] = useState(false);
+  const [showTransactionListModal, setShowTransactionListModal] = useState(false);
 
   return (
     <IonPage id="home-page">
@@ -33,7 +41,43 @@ const HomePage: React.FC = () => {
               <MainDisplayContainer period="Monthly" balance={234} spent={233} />
             </div>
           </IonSlide>
+
         </IonSlides>
+        <IonList ref={transactionListRef} lines="full">
+          <IonListHeader
+            ref={headerRef}
+            onClick={() => setShowTransactionListModal(true)}
+            id="transactionListHeader"
+            style={{ borderRadius: "10px 10px 0 0" }}
+          >
+            <h4 style={{ width: "85%" }}>Today</h4>
+            <span style={{ width: "15%" }}>
+              <IonIcon
+                icon={chevronUpOutline}
+                style={{ verticalAlign: "super" }}
+                color="medium"
+              />
+            </span>
+          </IonListHeader>
+
+          {transactions.length > 0 &&
+            transactions
+              .slice(0, 3)
+              .map((transaction, index) => (
+                <TransactionItem
+                  transaction={transaction}
+                  key={index}
+                />
+              ))}
+
+          {transactions.length === 0 && (
+            <IonItem>
+              <IonLabel>Add a transaction</IonLabel>
+            </IonItem>
+          )}
+        </IonList>
+
+
       </IonContent>
     </IonPage >
   );
