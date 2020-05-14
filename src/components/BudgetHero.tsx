b63dd8cc76ce3e3c51b8af48c524f6b8b288acb0
mzzlillieee <tanglillian11@gmail.com>
2020-05-14 09:56:08 -0700

    change the avocado hero in the BudgetHero component for demo

--- src/components/BudgetHero.tsx
+++ src/components/BudgetHero.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import avocado from '../theme/avocado.png'
+import avocado from '../theme/ketnip.png'
 import { StoreContext } from '../store'
 import './BudgetHero.scss';
 
@@ -15,7 +15,7 @@ const BudgetHero: React.FC<BudgetHeroProps> = () => {
     <div id="budget-hero">
       <div className='hero--wrapper'>
         <p>Daily Budget</p>
-        <h3>$35/day</h3>
+        <h3>$189/day</h3>
       </div>
       <img className='image--wrapper' src={avocado} alt="Logo" />
     </div>
