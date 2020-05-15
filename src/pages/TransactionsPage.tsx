637e5540c77759fccdb7e9a69cdc1585d76fdc33
mzzlillieee <tanglillian11@gmail.com>
2020-05-15 10:30:17 -0700

    no border to transaction header

--- src/pages/TransactionsPage.tsx
+++ src/pages/TransactionsPage.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './TransactionsPage.scss';
 
 const TransactionsPage: React.FC = () => {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <IonPage id="transactions-page">
       <IonHeader>
@@ -12,9 +14,12 @@ const TransactionsPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
+        <IonHeader className="ion-no-border" collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Transactions</IonTitle>
+          </IonToolbar>
+          <IonToolbar>
+            <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Transactions" />
