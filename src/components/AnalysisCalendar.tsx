23e1ef9e2487878c635d07f0a4f1e4d5a259a392
mzzlillieee <tanglillian11@gmail.com>
2020-05-19 10:56:54 -0700

    install moment library to get number of days in month and utilize their other methods for the AnalysisPage

--- src/components/AnalysisCalendar.tsx
+++ src/components/AnalysisCalendar.tsx
@@ -3,6 +3,7 @@ import avocado from '../theme/ketnip.png'
 import { StoreContext } from '../store'
 import { IonIcon } from '@ionic/react';
 import './AnalysisCalendar.scss'
+import moment from 'moment'
 import { waterOutline, water, waterSharp } from 'ionicons/icons';
 
 interface AnalysisCalendarProps {
@@ -12,7 +13,9 @@ interface AnalysisCalendarProps {
 const AnalysisCalendar: React.FC<AnalysisCalendarProps> = () => {
 
   const store = React.useContext(StoreContext);
-
+  const daysInCurrentMonth = moment().daysInMonth();
+  
+  console.log(daysInCurrentMonth)
   const may = ["overbudget", 'underbudget', 'underbudget', 'underbudget', 'underbudget', 'underbudget', 'underbudget', "overbudget", "overbudget", "overbudget", 'underbudget', 'underbudget', 'underbudget', 'underbudget', 'underbudget', "overbudget", 'underbudget', 'underbudget', 'today', 'future', 'future', 'future', "future", "future", "future", 'future', 'future', 'future', 'future', 'future']
 
   return (
