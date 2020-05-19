444f5cd49ada85e9e8c8a86445681b0d27a7d1d0
mzzlillieee <tanglillian11@gmail.com>
2020-05-18 20:18:37 -0700

    refactor the TransactionPage and make a TransactionList component and pass searchText as props

---
+++ src/components/TransactionList.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+  IonItem,
+  IonLabel,
+  IonList,
+  IonItemGroup,
+  IonItemDivider,
+} from "@ionic/react";
+import { StoreContext } from '../store';
+import TransactionItem from "./TransactionItem";
+
+interface TransactionListProps {
+  searchText: string;
+}
+
+const TransactionList: React.FC<TransactionListProps> = ({ searchText }) => {
+  const store = React.useContext(StoreContext);
+
+  const groupedTransactions = groupTransactionsByDate(store.transactions);
+  const filteredTransactionsBySearch = filteredTransactions(store.transactions, searchText);
+  const groupedFilteredTransactions = groupTransactionsByDate(filteredTransactionsBySearch)
+
+  return (
+    <IonList lines="full">
+      {(store.transactions.length > 0 && searchText !== "") &&
+        Object.keys(groupedFilteredTransactions).map((date: string, index: number) => (
+          <IonItemGroup key={`group-${index}`}>
+            <IonItemDivider sticky>
+              <IonLabel color='medium'>{date}</IonLabel>
+            </IonItemDivider>
+            {groupedFilteredTransactions[date].map((transaction: any, index: number) => (
+              <TransactionItem transaction={transaction} key={index} />
+            ))}
+          </IonItemGroup>
+        ))}
+
+      {(store.transactions.length > 0 && searchText === "") &&
+        Object.keys(groupedTransactions).map((date: string, index: number) => (
+          <IonItemGroup key={`group-${index}`}>
+            <IonItemDivider sticky>
+              <IonLabel color='medium'>{date}</IonLabel>
+            </IonItemDivider>
+            {groupedTransactions[date].map((transaction: any, index: number) => (
+              <TransactionItem transaction={transaction} key={index} />
+            ))}
+          </IonItemGroup>
+        ))}
+      {store.transactions.length === 0 && (
+        <IonItem>
+          <IonLabel>Add a transaction</IonLabel>
+        </IonItem>
+      )}
+    </IonList>
+
+  );
+};
+
+export default TransactionList;
+
+const groupTransactionsByDate = (transactions: any) => {
+  let groups: any = {};
+
+  const sortedTransactions = transactions.slice().sort((a: any, b: any) => Date.parse(b.transaction_time) - Date.parse(a.transaction_time));
+
+  sortedTransactions.map((transaction: any) => {
+    var date = new Date(transaction.transaction_time)
+    var dateString = date.toDateString().slice(4);
+    if (groups.hasOwnProperty(dateString)) {
+      groups[dateString].push(transaction)
+    } else {
+      groups[dateString] = [transaction]
+    }
+  })
+  return groups;
+}
+
+const filteredTransactions = (transactions: any, searchText: string) => {
+  return transactions.filter((transaction: any) => transaction.description.toLowerCase().indexOf(searchText.toLowerCase()) !== -1)
+}
--- src/pages/TransactionsPage.tsx
+++ src/pages/TransactionsPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar, IonList, IonListHeader, IonItem, IonLabel, IonItemGroup, IonItemDivider } from '@ionic/react';
 import './TransactionsPage.scss';
-import TransactionItem from '../components/TransactionItem';
+import TransactionList from '../components/TransactionList';
 import { StoreContext } from '../store';
 import { useObserver } from 'mobx-react';
 
@@ -10,9 +10,6 @@ const TransactionsPage: React.FC = () => {
   const store = React.useContext(StoreContext);
 
   const [searchText, setSearchText] = useState("");
-  const groupedTransactions = groupTransactionsByDate(store.transactions);
-  const filteredTransactionsBySearch = filteredTransactions(store.transactions, searchText);
-  const groupedFilteredTransactions = groupTransactionsByDate(filteredTransactionsBySearch)
 
   return useObserver(() => (
     <IonPage id="transactions-page">
@@ -30,37 +27,7 @@ const TransactionsPage: React.FC = () => {
             <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
           </IonToolbar>
         </IonHeader>
-
-        <IonList lines="full">
-          {(store.transactions.length > 0 && searchText !== "") &&
-            Object.keys(groupedFilteredTransactions).map((date: string, index: number) => (
-              <IonItemGroup key={`group-${index}`}>
-                <IonItemDivider sticky>
-                  <IonLabel color='medium'>{date}</IonLabel>
-                </IonItemDivider>
-                {groupedFilteredTransactions[date].map((transaction: any, index: number) => (
-                  <TransactionItem transaction={transaction} key={index} />
-                ))}
-              </IonItemGroup>
-            ))}
-
-          {(store.transactions.length > 0 && searchText === "") &&
-            Object.keys(groupedTransactions).map((date: string, index: number) => (
-              <IonItemGroup key={`group-${index}`}>
-                <IonItemDivider sticky>
-                  <IonLabel color='medium'>{date}</IonLabel>
-                </IonItemDivider>
-                {groupedTransactions[date].map((transaction: any, index: number) => (
-                  <TransactionItem transaction={transaction} key={index} />
-                ))}
-              </IonItemGroup>
-            ))}
-          {store.transactions.length === 0 && (
-            <IonItem>
-              <IonLabel>Add a transaction</IonLabel>
-            </IonItem>
-          )}
-        </IonList>
+        <TransactionList searchText={searchText} />
       </IonContent>
     </IonPage >
   ));
@@ -68,26 +35,3 @@ const TransactionsPage: React.FC = () => {
 
 export default TransactionsPage;
 
-const groupTransactionsByDate = (transactions: any) => {
-  let groups: any = {};
-
-  const sortedTransactions = transactions.slice().sort((a: any, b: any) => Date.parse(b.transaction_time) - Date.parse(a.transaction_time));
-
-  sortedTransactions.map((transaction: any) => {
-    var date = new Date(transaction.transaction_time)
-    var dateString = date.toDateString().slice(4);
-    if (groups.hasOwnProperty(dateString)) {
-      groups[dateString].push(transaction)
-    } else {
-      groups[dateString] = [transaction]
-    }
-  })
-
-
-  return groups;
-}
-
-
-const filteredTransactions = (transactions: any, searchText: string) => {
-  return transactions.filter((transaction: any) => transaction.description.toLowerCase().indexOf(searchText.toLowerCase()) !== -1)
-}
