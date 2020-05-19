a308f620c18f869adc3c25ea6866718b1d36f93a
mzzlillieee <tanglillian11@gmail.com>
2020-05-19 14:17:50 -0700

    implement the progress bar that dynamically indicates the current month in AnalysisHero component

--- src/components/AnalysisHero.tsx
+++ src/components/AnalysisHero.tsx
@@ -13,17 +13,20 @@ interface AnalysisHeroProps {
 const AnalysisHero: React.FC<AnalysisHeroProps> = () => {
 
   const store = React.useContext(StoreContext);
+  const daysInCurrentMonth = moment().daysInMonth();
+  const dayOfTheMonth = moment().date()
 
   function savingsThisMonth() {
-    const daysInCurrentMonth = moment().date()
     // ToDo: aggregate all the expenses for the current Month and make totalExpenseThisMonth dynamic
     let totalExpenseThisMonth = 1000
-    let budgetPerMonth = store.budget.budgetPerDay * daysInCurrentMonth;
+    let budgetPerMonth = store.budget.budgetPerDay * dayOfTheMonth;
     budgetPerMonth -= totalExpenseThisMonth
     return budgetPerMonth
   }
 
   const savingsSoFar = savingsThisMonth();
+  const numberOfDaysLeft = daysInCurrentMonth - dayOfTheMonth;
+
   return (
     <div id="analysis-hero">
       <div className='hero--wrapper'>
@@ -39,7 +42,7 @@ const AnalysisHero: React.FC<AnalysisHeroProps> = () => {
       <div className='progress-bar-wrapper'>
         <h1 style={{ display: "inline" }}>${savingsSoFar.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</h1><p style={{ display: "inline", color: 'var(--ion-color-tertiary)' }}> saved </p>
         <div className='progress-bar-light-grey'>
-          <div className='progress-bar-green' style={{ width: `65%`, color: 'white', textAlign: 'right' }}>10 days left</div>
+          <div className='progress-bar-green' style={{ width: `${dayOfTheMonth / daysInCurrentMonth * 100}%`, color: 'white', textAlign: 'right' }}>{numberOfDaysLeft} days left</div>
         </div>
       </div>
     </div>
