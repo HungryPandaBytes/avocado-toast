489d349fc3c598514c42e37b4fc50efd4b0d8a17
mzzlillieee <tanglillian11@gmail.com>
2020-05-19 14:09:38 -0700

    implement a function that calculate savings so far for the current month in AnalysisHero component

--- src/components/AnalysisHero.tsx
+++ src/components/AnalysisHero.tsx
@@ -3,6 +3,7 @@ import avocado from '../theme/ketnip.png'
 import { StoreContext } from '../store'
 import './AnalysisHero.scss';
 import { IonIcon } from '@ionic/react';
+import moment from 'moment'
 import { waterOutline, informationCircle, information, informationOutline } from 'ionicons/icons';
 
 interface AnalysisHeroProps {
@@ -13,7 +14,16 @@ const AnalysisHero: React.FC<AnalysisHeroProps> = () => {
 
   const store = React.useContext(StoreContext);
 
+  function savingsThisMonth() {
+    const daysInCurrentMonth = moment().daysInMonth();
+    // ToDo: aggregate all the expenses for the current Month and make totalExpenseThisMonth dynamic
+    let totalExpenseThisMonth = 1000
+    let budgetPerMonth = store.budget.budgetPerDay * daysInCurrentMonth;
+    budgetPerMonth -= totalExpenseThisMonth
+    return budgetPerMonth
+  }
 
+  const savingsSoFar = savingsThisMonth();
   return (
     <div id="analysis-hero">
       <div className='hero--wrapper'>
@@ -27,7 +37,7 @@ const AnalysisHero: React.FC<AnalysisHeroProps> = () => {
         />
       </span>
       <div className='progress-bar-wrapper'>
-        <h1 style={{ display: "inline" }}>$245</h1><p style={{ display: "inline", color: 'var(--ion-color-tertiary)' }}> saved </p>
+        <h1 style={{ display: "inline" }}>${savingsSoFar.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</h1><p style={{ display: "inline", color: 'var(--ion-color-tertiary)' }}> saved </p>
         <div className='progress-bar-light-grey'>
           <div className='progress-bar-green' style={{ width: `65%`, color: 'white', textAlign: 'right' }}>10 days left</div>
         </div>
