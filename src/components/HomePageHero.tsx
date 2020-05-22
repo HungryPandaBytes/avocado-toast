53702939cc5eb5039c1200fe5100e93363a4765f
mzzlillieee <tanglillian11@gmail.com>
2020-05-22 13:45:03 -0700

    show remaining time left for daily, weekly and monthly view

--- src/components/HomePageHero.tsx
+++ src/components/HomePageHero.tsx
@@ -3,6 +3,7 @@ import './HomePageHero.scss';
 import avocadoLogo from '../theme/ketnip.png'
 import burntToastLogo from '../theme/burnt-toast.png'
 import { StoreContext } from '../store';
+import moment from 'moment';
 
 interface HomePageHeroProps {
   period: string;
@@ -17,10 +18,13 @@ const HomePageHero: React.FC<HomePageHeroProps> = ({ period, balance, spent }) =
   const progressbarStyling = balance > 0 ? "progress-bar-green" : "progress-bar-red"
   const progressBarPercentage = Math.ceil(spent / (balance + spent) * 100)
   const mascot = balance > 0 ? avocadoLogo : burntToastLogo
-
+  const today = moment();
+  const timeLeftToday = moment().endOf('day').to(today, true);
+  const daysLeftThisWeek = moment().endOf('isoWeek').to(today, true);
+  const daysLeftThisMonth = moment().endOf('month').to(today, true);
 
   return (
-    <div id="main-display-container" style={{textAlign: "left"}}>
+    <div id="main-display-container" style={{ textAlign: "left" }}>
       <h1>{period}</h1>
       <div className={displayStyling}>
         <div className='mascot-wrapper'>
@@ -33,7 +37,12 @@ const HomePageHero: React.FC<HomePageHeroProps> = ({ period, balance, spent }) =
           <div className='progress-bar-light-grey'>
             <div className={progressbarStyling} style={{ width: `${progressBarPercentage}%` }}></div>
           </div>
-          {<p>${spent} spent</p>}
+          <div className='bottom-display-wrapper'>
+            {(period === 'Daily') && <div>{timeLeftToday} left</div>}
+            {(period === 'Weekly') && <div>{daysLeftThisWeek} left</div>}
+            {(period === 'Monthly') && <div>{daysLeftThisMonth} left</div>}
+            <div>${spent} spent</div>
+          </div>
         </div>
       </div>
     </div>
