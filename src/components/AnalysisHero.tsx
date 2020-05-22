b46bb6e481de24f16fd81125ddc294e28f01e049
mzzlillieee <tanglillian11@gmail.com>
2020-05-22 13:50:23 -0700

    refactor how many days left on AnalysisHero page

--- src/components/AnalysisHero.tsx
+++ src/components/AnalysisHero.tsx
@@ -13,8 +13,11 @@ interface AnalysisHeroProps {
 const AnalysisHero: React.FC<AnalysisHeroProps> = () => {
 
   const store = React.useContext(StoreContext);
+  const today = moment()
   const daysInCurrentMonth = moment().daysInMonth();
   const dayOfTheMonth = moment().date()
+  const daysLeftThisMonth = moment().endOf('month').to(today, true);
+
 
   function savingsThisMonth() {
     // ToDo: aggregate all the expenses for the current Month and make totalExpenseThisMonth dynamic
@@ -25,7 +28,6 @@ const AnalysisHero: React.FC<AnalysisHeroProps> = () => {
   }
 
   const savingsSoFar = savingsThisMonth();
-  const numberOfDaysLeft = daysInCurrentMonth - dayOfTheMonth;
 
   return (
     <div id="analysis-hero">
@@ -42,7 +44,7 @@ const AnalysisHero: React.FC<AnalysisHeroProps> = () => {
       <div className='progress-bar-wrapper'>
         <h1 style={{ display: "inline" }}>${savingsSoFar.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</h1><p style={{ display: "inline", color: 'var(--ion-color-tertiary)' }}> saved this month</p>
         <div className='progress-bar-light-grey'>
-          <div className='progress-bar-green' style={{ width: `${dayOfTheMonth / daysInCurrentMonth * 100}%`, color: 'white', textAlign: 'right', padding: '1% 3% 0 0' }}>{numberOfDaysLeft} days left</div>
+          <div className='progress-bar-green' style={{ width: `${dayOfTheMonth / daysInCurrentMonth * 100}%`, color: 'white', textAlign: 'right', padding: '1% 3% 0 0' }}>{daysLeftThisMonth} left</div>
         </div>
       </div>
     </div>
