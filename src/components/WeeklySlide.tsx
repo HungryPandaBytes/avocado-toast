5be8d9e3e53efb3753423b9b53bc7e66d05b05ad
Lorraine Chan <lchan912@gmail.com>
2020-05-25 15:45:21 -0700

    WIP - categories display

--- src/components/WeeklySlide.tsx
+++ src/components/WeeklySlide.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { IonList, IonItem, IonLabel } from "@ionic/react";
+import { IonList, IonItem, IonLabel, IonIcon } from "@ionic/react";
 import { Transaction } from "../models/Transaction";
+import { basketOutline } from "ionicons/icons";
 
 const loadCategories = (transactions: Transaction[]): {[key: string]: number} => {
   const categories: { [key: string]: number } = {};
@@ -24,12 +25,16 @@ const WeeklySlide: React.FC<WeeklySlideProps> = ({ transactions }) => {
   const categories: {[key: string]: number} = loadCategories(transactions);
 
   return (
-    <IonList>
+    <IonList lines="none">
       {Object.keys(categories).map((category_name: string) => {
         return (
           <IonItem key={category_name}>
-            <IonLabel>
-              {category_name}: {categories[category_name]}
+            <IonIcon icon={basketOutline} slot="start"></IonIcon>
+            <IonLabel slot="start">
+              {category_name}
+            </IonLabel>
+            <IonLabel slot="end">
+              {categories[category_name]}
             </IonLabel>
           </IonItem>
         );
