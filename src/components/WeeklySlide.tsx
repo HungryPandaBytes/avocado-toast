6733904f873409efab0bd053f9cf9f470da3541d
Lorraine Chan <lchan912@gmail.com>
2020-05-25 22:46:35 -0700

    add categories to weeklyslides

--- src/components/WeeklySlide.tsx
+++ src/components/WeeklySlide.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { IonList, IonItem, IonLabel, IonIcon } from "@ionic/react";
+import { IonList, IonItem, IonLabel, IonIcon, IonNote, IonItemGroup } from "@ionic/react";
 import { Transaction } from "../models/Transaction";
 import { basketOutline } from "ionicons/icons";
+import './WeeklySlide.scss';
 
 const loadCategories = (transactions: Transaction[]): {[key: string]: number} => {
-  const categories: { [key: string]: number } = {};
+  const categories: { [key: string]: number } = {total: 0};
+
+  transactions.sort((a: Transaction, b: Transaction) => b.amount - a.amount)
 
   transactions.forEach((transaction: Transaction) => {
     if (categories.hasOwnProperty(transaction.category_name)) {
@@ -12,6 +15,7 @@ const loadCategories = (transactions: Transaction[]): {[key: string]: number} =>
     } else {
       categories[transaction.category_name] = transaction.amount;
     }
+    categories.total += transaction.amount;
   });
 
   return categories;
@@ -23,20 +27,35 @@ interface WeeklySlideProps {
 
 const WeeklySlide: React.FC<WeeklySlideProps> = ({ transactions }) => {
   const categories: {[key: string]: number} = loadCategories(transactions);
+  let categoryTotal = 0;
+  let maxWidth = 90;
 
   return (
-    <IonList lines="none">
+    <IonList lines="none" id="weekly__slide">
       {Object.keys(categories).map((category_name: string) => {
+        if (category_name === "total") return;
+
+        categoryTotal = categories[category_name];
+
         return (
-          <IonItem key={category_name}>
-            <IonIcon icon={basketOutline} slot="start"></IonIcon>
-            <IonLabel slot="start">
-              {category_name}
-            </IonLabel>
-            <IonLabel slot="end">
-              {categories[category_name]}
-            </IonLabel>
-          </IonItem>
+          <div key={category_name} className="categories__container">
+            <IonItem class="category__item">
+              <IonIcon icon={basketOutline} slot="start" color="primary"></IonIcon>
+              <IonLabel>{category_name}</IonLabel>
+
+              <IonLabel slot="end" style={{ textAlign: "right" }}>
+                {categoryTotal}
+              </IonLabel>
+            </IonItem>
+            <div className="categories__progress-bar" style={{ width: maxWidth + "%" }}>
+              <div
+                className="categories__progress-bar--inner"
+                style={{
+                  width: (((categoryTotal / categories.total) * maxWidth) / maxWidth) * 100 + "%",
+                }}
+              ></div>
+            </div>
+          </div>
         );
       })}
     </IonList>
