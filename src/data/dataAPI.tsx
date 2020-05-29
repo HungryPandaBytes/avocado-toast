aecca595ede5de4a754c3b21b00cc7d6641273bf
mzzlillieee <tanglillian11@gmail.com>
2020-05-29 15:01:33 -0700

    clear some of the unnecessary logs

--- src/data/dataAPI.tsx
+++ src/data/dataAPI.tsx
@@ -8,16 +8,13 @@ import { v4 as uuidv4 } from 'uuid';
 // get all transactions from db  
 export const loadTransactions = async () => {
   const DBstore = new DBService();
-  console.log(DBstore);
   await DBstore.initPlugin();
   let result = await DBstore.openStore({ database: 'avocado-toast', table: 'transactions' });
-
   if (result) {
-    console.log('Avocado-Toast DB open', result);
     // get all transactions
     const allTransactions = await DBstore.getAllValues();
     let parsedResult = await allTransactions.map((transaction: string) => JSON.parse(transaction));
-    console.log(parsedResult)
+    console.log('fetching all transactions from db', parsedResult)
     return parsedResult;
   }
 }
@@ -25,24 +22,21 @@ export const loadTransactions = async () => {
 // add a new transaction to db
 export const addNewTransactionToDB = async (newTransaction: Transaction) => {
   const DBstore = new DBService();
-  console.log(DBstore);
   await DBstore.initPlugin();
   let result = await DBstore.openStore({ database: 'avocado-toast', table: 'transactions' });
   if (result) {
     await DBstore.setItem(newTransaction.id, JSON.stringify(newTransaction));
-    console.log(`Added a new transaction of ${newTransaction.category_name} to Avocado-Toast DB`)
+    console.log(`Added a new transaction of ${newTransaction.category_name} of ${newTransaction.amount} to Avocado-Toast DB`)
   }
 }
 
 // seed database 
 export const seedDatabase = async () => {
   const DBstore = new DBService();
-  console.log(DBstore);
   await DBstore.initPlugin();
   let result = await DBstore.openStore({ database: 'avocado-toast' });
   await DBstore.clear();
   if (result) {
-    console.log('avocado-toast DB open', result);
     /***************************************
      * Open "avocado-toast" and table "transactions" *
      ***************************************/
